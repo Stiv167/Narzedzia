dee6d3dfb028667f0433b3b89cf3ecdbd5d28930
Filip <cehakfilip@gmail.com>
2022-02-13 15:04:09 +0100

    zmiana nazwy zmiennej filltable na polska

--- funkcja+tablica.cpp
+++ funkcja+tablica.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 #define N 10
 
-int fillTable(int t[], int n) {
+int uzupelnijTabele(int t[], int n) {
     int size = t[0];
     for (size_t i = 0; i < n; i++) {
         if (t[1] > size) {
@@ -20,6 +20,6 @@ int main()
         cout << "Podaj liczbe na pozycje: " << i << ": ";
         cin >> tab[i];
     }
-    cout << "Najwieksza wartosc z tablicy: " << fillTable(tab, N) << endl;
-    cout << "Najwieksza wartosc z polowy tablicy: " << fillTable(tab, N / 2);
+    cout << "Najwieksza wartosc z tablicy: " << uzupelnijTabele(tab, N) << endl;
+    cout << "Najwieksza wartosc z polowy tablicy: " << uzupelnijTabele(tab, N / 2);
 }
