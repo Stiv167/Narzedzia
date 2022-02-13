1edc792371d12fbc23d7b5ed29e595030a81ab65
Filip <cehakfilip@gmail.com>
2022-02-13 15:01:01 +0100

    dodanie glownego programu

---
+++ funkcja+tablica.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+#define N 10
+
+int fillTable(int t[], int n) {
+    int size = t[0];
+    for (size_t i = 0; i < n; i++) {
+        if (t[1] > size) {
+            size = t[1];
+        }
+    }
+    return size;
+}
+
+int main()
+{
+    int tab[N];
+    for (size_t i = 0; i < N; i++) {
+        cout << "Podaj liczbe na pozycje: " << i << ": ";
+        cin >> tab[i];
+    }
+    cout << "Najwieksza wartosc z tablicy: " << fillTable(tab, N) << endl;
+    cout << "Najwieksza wartosc z polowy tablicy: " << fillTable(tab, N / 2);
+}
